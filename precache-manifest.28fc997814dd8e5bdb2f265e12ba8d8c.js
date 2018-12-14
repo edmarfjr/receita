06fac11ce99bebed6237659a7fe092cb957e2954
edmarfjr <edmarjr@outlook.com>
2018-12-14 11:55:11 -0200

    Updates

---
+++ precache-manifest.28fc997814dd8e5bdb2f265e12ba8d8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "528574df4faa550a516c",
+    "url": "/receita/static/js/runtime~main.528574df.js"
+  },
+  {
+    "revision": "82f834a14c75e355d432",
+    "url": "/receita/static/js/main.82f834a1.chunk.js"
+  },
+  {
+    "revision": "733b3866e56f33742b20",
+    "url": "/receita/static/js/1.733b3866.chunk.js"
+  },
+  {
+    "revision": "82f834a14c75e355d432",
+    "url": "/receita/static/css/main.e32fca61.chunk.css"
+  },
+  {
+    "revision": "89ca60e7f24535ce85ea1fd0c228de4b",
+    "url": "/receita/index.html"
+  }
+];
